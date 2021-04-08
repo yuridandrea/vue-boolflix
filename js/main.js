f836b0790ea564742560f76a4519319a2a889720
yuri dandrea <dandreaayuri@gmail.com>
2021-04-08 18:05:14 +0200

    bonus in corso

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var app = new Vue({
   el: "#app",
   data: {
-    movies: [],
-    series: [],
+    results: [],
+    bests: [],
     flags: ["it", "en"],
     search: "",
   },
@@ -10,24 +10,17 @@ var app = new Vue({
     ask: function () {
       axios.get("https://api.themoviedb.org/3/search/movie?api_key=fab054eb96530e8d5e0eea953e08c0c3&language=it-IT", {params : {query : this.search }})
       .then((result) => {
-        // result.data.results.forEach(x =>{
-        //   console.log(x)
-        //   // x.vote_average = Math.round(x.vote_average / 2)
-        // })
-        this.movies = result.data.results
+        this.results = result.data.results
         this.search = ""
-        console.log(this.movies)
+        console.log(this.results)
        
       })
       axios.get("https://api.themoviedb.org/3/search/tv?api_key=fab054eb96530e8d5e0eea953e08c0c3&language=it-IT", {params : {query : this.search }})
       .then((result) => {
-        // result.data.results.forEach(x =>{
-        //   console.log(x)
-        //   // x.vote_average = Math.round(x.vote_average / 2)
-        // })
-        this.series = result.data.results;
+        const results = result.data.results;
+        this.results = this.results.concat(results);
         this.search = "";
-        console.log(this.series)
+        console.log(this.results)
       })
     },
     vote(vote){
@@ -37,7 +30,17 @@ var app = new Vue({
       return `img/${lang}.png`;
     },
     getPoster(poster) {
-      return `http://image.tmdb.org/t/p/w185/${poster}`
+      return `http://image.tmdb.org/t/p/w185/${poster}`;
+    },
+    clean() {
+      return this.results = []
     }
+  },
+  mounted() {
+    axios.get("https://api.themoviedb.org/3/trending/movie/day?api_key=fab054eb96530e8d5e0eea953e08c0c3")
+    .then((result) => {
+      this.bests = result.data.results
+      console.log(this.bests)
+    })
   }
 });
